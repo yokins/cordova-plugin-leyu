1f20958988e87c5451d26510af2d063b24dc4937
yokins <yokins.shi@icloud.com>
2019-10-15 11:52:56 +0800

    修改代码

--- src/android/LeYu.java
+++ src/android/LeYu.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.content.ServiceConnection;
@@ -20,25 +21,6 @@ import aidl.com.leyu.LeyuService;
  * This class echoes a string called from JavaScript.
  */
 public class LeYu extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        if (action.equals("getTpEnable")) {
-            this.getTpEnable();
-            return true;
-        }
-        if (action.equals("setTpEnable")) {
-            this.setTpEnable(true);
-            return true;
-        }
-        return false;
-    }
-
     private LeyuService leyuService;
     private boolean connected = false;
     private ServiceConnection connection = new ServiceConnection() {
@@ -66,29 +48,141 @@ public class LeYu extends CordovaPlugin {
         connected = context.bindService(intent, connection, context.BIND_AUTO_CREATE);
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        attemptToBindService();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(connection!=null) {
+            unbindService(connection);
+            connection=null;
+        }
+        super.onDestroy();
+    }
+
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // if (action.equals("coolMethod")) {
+        //     String message = args.getString(0);
+        //     this.coolMethod(message, callbackContext);
+        //     return true;
+        // }
+        if (action.equals("getTpEnable")) {
+            this.getTpEnable(callbackContext);
+            return true;
+        }
+        if (action.equals("setTpEnable")) {
+            JSONObject jsonObject = args.getJSONObject(0);
+            boolean enable = jsonObject.getBoolean("enable");
+            this.setTpEnable(enable, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    // private void coolMethod(String message, CallbackContext callbackContext) {
+    //     if (message != null && message.length() > 0) {
+    //         callbackContext.success(message);
+    //     } else {
+    //         callbackContext.error("Expected one non-empty string argument.");
+    //     }
+    // }
+
+    public void getTpEnable(CallbackContext callbackContext){
+        try {
+            boolean enable = leyuService.getTpEnable();
+            callbackContext.success({ message: "获取乐愚状态成功！", enable: enable });
+        } catch (RemoteException e) {
+            callbackContext.error("获取乐愚状态失败！");
+            e.printStackTrace();
         }
     }
 
-    private void setTpEnable(boolean enable) {
+    public void setTpEnable(boolean enable, CallbackContext callbackContext){
         try {
             leyuService.setTpEnable(enable);
+            callbackContext.success({ message: "设置乐愚状态成功！", enable: enable });
         } catch (RemoteException e) {
+            callbackContext.error("设置乐愚状态失败！");
             e.printStackTrace();
         }
     }
 
-    private boolean getTpEnable() {
-        boolean enable = false;
-        try {
-            enable = leyuService.getTpEnable();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return enable;
-    }
+
+
+    // @Override
+    // public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    //     if (action.equals("coolMethod")) {
+    //         String message = args.getString(0);
+    //         this.coolMethod(message, callbackContext);
+    //         return true;
+    //     }
+    //     if (action.equals("getTpEnable")) {
+    //         this.getTpEnable();
+    //         return true;
+    //     }
+    //     if (action.equals("setTpEnable")) {
+    //         this.setTpEnable(true);
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // private LeyuService leyuService;
+    // private boolean connected = false;
+    // private ServiceConnection connection = new ServiceConnection() {
+
+    //     @Override
+    //     public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+    //         leyuService = LeyuService.Stub.asInterface(iBinder); 
+    //         if(leyuService!=null) {
+    //             connected = true; 
+    //         } else {
+    //             connected =false; 
+    //         }
+    //     }
+
+    //     @Override
+    //     public void onServiceDisconnected(ComponentName componentName) {
+    //         connected = false; 
+    //     }
+    // };
+
+    // private void attemptToBindService(Context context) {
+    //     Intent intent = new Intent();
+    //     intent.setComponent(new
+    //             ComponentName("com.android.settings", "com.android.settings.LeyuInterfaceService"));
+    //     connected = context.bindService(intent, connection, context.BIND_AUTO_CREATE);
+    // }
+
+    // private void coolMethod(String message, CallbackContext callbackContext) {
+    //     if (message != null && message.length() > 0) {
+    //         callbackContext.success(message);
+    //     } else {
+    //         callbackContext.error("Expected one non-empty string argument.");
+    //     }
+    // }
+
+    // private void setTpEnable(boolean enable) {
+    //     try {
+    //         leyuService.setTpEnable(enable);
+    //     } catch (RemoteException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    // private boolean getTpEnable() {
+    //     boolean enable = false;
+    //     try {
+    //         enable = leyuService.getTpEnable();
+    //     } catch (RemoteException e) {
+    //         e.printStackTrace();
+    //     }
+    //     return enable;
+    // }
 }
--- www/leyu.js
+++ www/leyu.js
@@ -1,20 +1,49 @@
 var exec = require('cordova/exec');
 
 const leyu = {
-  coolMethod: function(arg0, success, error) {
-    exec(success, error, 'LeYu', 'coolMethod', [arg0]);
-    console.log('coolMethod, in');
+  // coolMethod: function(arg0, success, error) {
+  //   exec(success, error, 'LeYu', 'coolMethod', [arg0]);
+  //   console.log('coolMethod, in');
+  // },
+
+  // 设置
+  setTpEnable: function(enable) {
+    console.info('---leyu---setTpEnable---');
+    return new Promise(resolve => {
+      exec(
+        success => {
+          console.info(`${success.message}：` + success.enable);
+          resolve({ status: true });
+        },
+        error => {
+          console.info(error);
+          resolve({ status: false });
+        },
+        'LeYu',
+        'setTpEnable',
+        [{ enable: enable }]
+      );
+    });
   },
 
-  setTpEnable: function(arg0) {
-    console.log(arg0);
-    console.log('setTpEnable, in');
-    exec(() => {}, () => {}, 'LeYu', 'setTpEnable', [arg0]);
-  },
-
+  // 获取状态
   getTpEnable: function() {
-    console.log('getTpEnable, in');
-    exec(() => {}, () => {}, 'LeYu', 'getTpEnable', '');
+    console.info('---leyu---getTpEnable---');
+    return new Promise(resolve => {
+      exec(
+        success => {
+          console.info(`${success.message}：` + success.enable);
+          resolve({ status: true, enable: enable });
+        },
+        error => {
+          console.info(error);
+          resolve({ status: false });
+        },
+        'LeYu',
+        'getTpEnable',
+        []
+      );
+    });
   }
 };
 
