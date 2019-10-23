3f2717820fe8f03ab17226f7853bb48f6e1e2c79
yokins <yokins.shi@icloud.com>
2019-10-23 17:53:17 +0800

    修改方式

--- src/android/LeYu.old.java
+++ src/android/LeYu.old.java
@@ -1,45 +1,44 @@
 package com.yokins;
 
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.util.Log;
-import android.webkit.WebView;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebSettings;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
-
-import org.apache.cordova.*;
+import android.content.ServiceConnection;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.Context;
 
 import aidl.com.leyu.LeyuService;
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class LeYu extends CordovaPlugin implements CordovaActivity {
+public class LeYu extends CordovaPlugin {
+    private static Context context;
     private LeyuService leyuService;
-    private boolean connected =false;
+    private boolean connected = false;
     private ServiceConnection connection = new ServiceConnection() {
+
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            leyuService = LeyuService.Stub.asInterface(iBinder);
-            if (leyuService != null) {
-                connected = true;
+            leyuService = LeyuService.Stub.asInterface(iBinder); 
+            if(leyuService!=null) {
+                connected = true; 
             } else {
-                connected = false;
+                connected =false; 
             }
-            Log.i(TAG, "yokins-log connected:" + connected);
         }
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
-            connected = false;
+            connected = false; 
         }
     };
 
@@ -47,31 +46,15 @@ public class LeYu extends CordovaPlugin implements CordovaActivity {
         Intent intent = new Intent();
         intent.setComponent(new
                 ComponentName("com.android.settings", "com.android.settings.LeyuInterfaceService"));
-        connected = this.bindService(intent , connection ,BIND_AUTO_CREATE);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
-            moveTaskToBack(true);
-        }
-        loadUrl(launchUrl);
-        attemptToBindService();
-    }
-
-    @Override
-    public void onDestroy() {
-        if(connection!=null) {
-            unbindService(connection);
-            connection=null;
-        }
+        connected = context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("openTp")) {
+            this.attemptToBindService();
+            return true;
+        }
         if (action.equals("getTpEnable")) {
             this.getTpEnable(callbackContext);
             return true;
