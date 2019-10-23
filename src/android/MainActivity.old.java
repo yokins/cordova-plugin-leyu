33a849235005d82f01835d112fc9c2efb8417b9a
yokins <yokins.shi@icloud.com>
2019-10-23 16:05:45 +0800

    阐释

--- src/android/MainActivity.old.java
+++ src/android/MainActivity.old.java
@@ -1,25 +1,26 @@
 package com.yokins;
-
 import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
-import org.apache.cordova.*;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.widget.Toast;
 
 import aidl.com.leyu.LeyuService;
 
-public class MainActivity extends CordovaActivity
-{
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
 
     private LeyuService leyuService;
     private boolean connected =false;
@@ -44,46 +45,38 @@ public class MainActivity extends CordovaActivity
         Intent intent = new Intent();
         intent.setComponent(new
                 ComponentName("com.android.settings", "com.android.settings.LeyuInterfaceService"));
-        connected = this.bindService(intent , connection ,BIND_AUTO_CREATE);
+        connected = this.bindService(intent , connection , BIND_AUTO_CREATE);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
-            moveTaskToBack(true);
-        }
-        loadUrl(launchUrl);
+        setContentView(R.layout.activity_main);
         initView();
         attemptToBindService();
     }
 
     @Override
-    public void onDestroy() {
+    protected void onDestroy() {
         if(connection!=null) {
             unbindService(connection);
             connection=null;
         }
+        super.onDestroy();
     }
-    
+
     private void initView(){
-        //mWebView = findViewById(R.id.web);
+        mWebView = findViewById(R.id.web);
         // 得到设置属性的对象
-        //WebSettings webSettings = mWebView.getSettings();
-        WebSettings webSettings = appView.getSettings();
-        // 使能JavaScript
-        //webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptEnabled(true);
-        // 载入页面：本地html资源文件
-        //mWebView.loadUrl("file:///android_asset/sample.html");
-        // WebViewClient 主要帮助WebView处理各种通知、请求事件的
-        //mWebView.setWebViewClient(new WebViewClient());
-        appView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        // // 使能JavaScript
+        // webSettings.setJavaScriptEnabled(true);
+        // // 载入页面：本地html资源文件
+        // mWebView.loadUrl("file:///android_asset/sample.html");
+        // // WebViewClient 主要帮助WebView处理各种通知、请求事件的
+        // mWebView.setWebViewClient(new WebViewClient());
         // 传入一个Java对象和一个接口名,在JavaScript代码中用这个接口名代替这个对象,通过接口名调用Android接口的方法
-        //mWebView.addJavascriptInterface(new MainActivity.MyJsInteration(this),"Android");
-        appView.addJavascriptInterface(new MyJsInteration(this),"Android");
+        mWebView.addJavascriptInterface(new MyJsInteration(this),"Android");
     }
 
     public  class  MyJsInteration {
@@ -114,11 +107,9 @@ public class MainActivity extends CordovaActivity
             try {
                 boolean enable = leyuService.getTpEnable();
                 if(enable){
-                    //Toast.makeText(mContext, "当前状态:禁止手指", Toast.LENGTH_SHORT).show();
-                    Log.i(TAG, "enable finger");
+                    Toast.makeText(mContext, "当前状态:禁止手指", Toast.LENGTH_SHORT).show();
                 }else {
-                    //Toast.makeText(mContext, "当前状态:允许手指", Toast.LENGTH_SHORT).show();
-                    Log.i(TAG, "disable finger");
+                    Toast.makeText(mContext, "当前状态:允许手指", Toast.LENGTH_SHORT).show();
                 }
             } catch (RemoteException e) {
                 e.printStackTrace();
