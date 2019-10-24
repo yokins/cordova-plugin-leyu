281aeae3cd143280209acefc764acba1c5befa47
yokins <yokins.shi@icloud.com>
2019-10-24 10:55:13 +0800

    修改调用方式

--- www/LeyuPlugin.js
+++ www/LeyuPlugin.js
@@ -1,50 +1,74 @@
 var exec = require('cordova/exec');
 
 const leyu = {
+  /**
+   * @description: 开启服务
+   * @param {type}
+   * @return:
+   */
+  openService: function() {
+    exec(() => {}, () => {}, 'LeyuPlugin', 'attemptToBindService', []);
+  },
+  /**
+   * @description: 关闭服务
+   * @param {type}
+   * @return:
+   */
+  closeService: function() {
+    exec(() => {}, () => {}, 'LeyuPlugin', 'unbindService', []);
+  },
+  /**
+   * @description: 获取状态
+   * @param {type}
+   * @return:
+   */
+  getTpEnable: function() {
+    exec(() => {}, () => {}, 'LeyuPlugin', 'getTpEnable', []);
+  },
+  /**
+   * @description: 设置状态
+   * @param {type}
+   * @return:
+   */
+  setTpEnable: function(enable) {
+    exec(() => {}, () => {}, 'LeyuPlugin', 'setTpEnable', [enable]);
+  }
   // coolMethod: function(arg0, success, error) {
   //   exec(success, error, 'LeYu', 'coolMethod', [arg0]);
   //   console.log('coolMethod, in');
   // },
-
-  openTp: function() {
-    exec(()=> {}, () => {}, 'LeYu', 'openTp', [])
-  },
-
-  // 设置
-  setTpEnable: function(enable) {
-    console.info('---leyu---setTpEnable---');
-    return new Promise(resolve => {
-      exec(
-        () => {
-          resolve({ status: true });
-        },
-        () => {
-          resolve({ status: false });
-        },
-        'LeYu',
-        'setTpEnable',
-        [{ enable: enable }]
-      );
-    });
-  },
-
-  // 获取状态
-  getTpEnable: function() {
-    console.info('---leyu---getTpEnable---');
-    return new Promise(resolve => {
-      exec(
-        enable => {
-          resolve({ status: true, enable: enable });
-        },
-        () => {
-          resolve({ status: false });
-        },
-        'LeYu',
-        'getTpEnable',
-        []
-      );
-    });
-  }
+  // setTpEnable: function(enable) {
+  //   console.info('---LeyuPlugin---setTpEnable---');
+  //   return new Promise(resolve => {
+  //     exec(
+  //       () => {
+  //         resolve({ status: true });
+  //       },
+  //       () => {
+  //         resolve({ status: false });
+  //       },
+  //       'LeyuPlugin',
+  //       'setTpEnable',
+  //       [{ enable: enable }]
+  //     );
+  //   });
+  // },
+  // getTpEnable: function() {
+  //   console.info('---LeyuPlugin---getTpEnable---');
+  //   return new Promise(resolve => {
+  //     exec(
+  //       enable => {
+  //         resolve({ status: true, enable: enable });
+  //       },
+  //       () => {
+  //         resolve({ status: false });
+  //       },
+  //       'LeyuPlugin',
+  //       'getTpEnable',
+  //       []
+  //     );
+  //   });
+  // }
 };
 
 module.exports = leyu;
